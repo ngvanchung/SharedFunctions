f0b013a6478dd0fc935ad05fddc8470dea182333
ngvanchung <nguyenvanchung85@gmail.com>
2020-08-16 22:20:15 +0700

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -1,44 +1,16 @@
-import java.io.*;
-import groovy.io.*;
-import java.util.Calendar.*;
-import java.text.SimpleDateFormat
-import hudson.model.*
+import java.io.*
+import groovy.io.*
 
-@NonCPS
-def call(Map config=[:]){
-    def dir = new File(pwd());
-    
-    new File(dir.path + '/releasenotes.txt').withWriter('utf-8') 
-    { 
-    	writer -> 
-                dir.eachFileRecurse(FileType.ANY){ file ->
-    		if (file.isDirectory()){
-    			writer.writeLine(file.name);            
-    		}
-    		else
-    		{
-    			writer.writeLine('\t' + file.name + '\t' + file.length());
-    		}
-           }
-    
-        def date = new Date()
-        def sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss")
-        writer.writeLine("Date and Time IS: " + sdf.format(date));
-    
-        writer.writeLine("Build Number is: ${BUILD_NUMBER}");
-        
-        def changeLogSets = currentBuild.changeSets;
-        
-        if (config.changes != "false"){
-            for (change in changeLogSets) {
-            	def entries = change.items;
-            	for (entry in entries) {
-            		writer.writeLine("${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}")
-            		for (file in entry.affectedFiles) {
-            			writer.writeLine("${file.editType.name} ${file.path}");
-            		}
-            	}
-            }    
-        }    
+def call(Map config=[:]) {
+    def dir = 
+    new File(dir.path + "/releasenotes.txt").withWriter('utf-8') {
+        writer ->
+            dir.eachFileRecurse(FileType.ANY) {file ->
+                if (file.isDirectory()) {
+                    writer.writeLine(file.name)
+                } else {
+                    writer.writeLine(file.name + '\t' + file.length())
+                }
+            }
     }
 }
