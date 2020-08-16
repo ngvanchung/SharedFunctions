beef053f4cb4bd17f940b2047b776f0d19dc367b
ngvanchung <nguyenvanchung85@gmail.com>
2020-08-16 22:25:52 +0700

    Update releasenotes.groovy

--- vars/releasenotes.groovy
+++ vars/releasenotes.groovy
@@ -2,7 +2,7 @@ import java.io.*
 import groovy.io.*
 
 def call(Map config=[:]) {
-    def dir = 
+    def dir = new File(pwd());
     new File(dir.path + "/releasenotes.txt").withWriter('utf-8') {
         writer ->
             dir.eachFileRecurse(FileType.ANY) {file ->
